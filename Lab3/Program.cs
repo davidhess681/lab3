96af0f439522319bac89f531bc7cd0c24312e2a4
david <david.hess681@gmail.com>
2018-10-09 16:32:19 -0400

    Organized by methods; improved comments

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -8,82 +8,105 @@ namespace Lab3
 {
     class Program
     {
-
+        // Declaring Variables
+        static string username;
+        static string userInput;
+        static int userNum;
+        static bool cont = true;
 
         static void Main(string[] args)
         {
-            // Declaring Variables
-            string username;
-            string userInput;
-            int userNum;
-            bool cont = true;
+            // Start with user's name
+            UserName();
+            do
+            {
+                // Run the methods in this order
+                Console.WriteLine("Enter a number between 1 and 100:");
+                ValidateInput();
+                Conditions();
+                Continue();
+            }
+            // End program if cont is false
+            while (cont);
+        }
 
+        static void UserName()
+        {
             // Get Username
             Console.WriteLine("Hello! What is your name?");
             username = Console.ReadLine();
             Console.WriteLine("");
             Console.WriteLine("Hi {0}, let's get started!", username);
+        }
 
-            do
+        static void ValidateInput()
+        {
+            // Check if user input is an integer
+            userInput = Console.ReadLine();
+            while (!int.TryParse(userInput, out int result))
             {
-                Console.WriteLine("Enter a number between 1 and 100:");
-
-                // Validate User Input
+                Console.WriteLine("Invalid input. Try again:");
                 userInput = Console.ReadLine();
-                while (!int.TryParse(userInput, out int result))
-                {
-                    Console.Write("Invalid input. Try again:");
-                    userInput = Console.ReadLine();
-                }
-                userNum = int.Parse(userInput);
-                Console.WriteLine("");
+            }
+            // Apply input to the int userNum
+            userNum = int.Parse(userInput);
+            Console.WriteLine("");
+        }
 
-                // Conditions (if statements)
-                if (userNum <= 100 && userNum > 0)
+        static void Conditions()
+        {
+            // Check if input is between 1 and 100
+            if (userNum <= 100 && userNum > 0)
+            {
+                // Check if odd or even
+                if (userNum % 2 == 1)
                 {
-                    if (userNum % 2 == 1)
+                    // The results for >60 and <60 are the same but the if statement is here anyways
+                    if (userNum > 60)
                     {
-                        if (userNum > 60)
-                        {
-                            Console.WriteLine("The number {0} is odd", userNum);
-                        }
-                        else
-                        {
-                            Console.WriteLine("The number {0} is odd", userNum);
-                        }
+                        Console.WriteLine("The number {0} is odd", userNum);
                     }
                     else
                     {
-                        if (userNum < 25)
-                        {
-                            Console.WriteLine("Even and less than 25");
-                        }
-                        else if (userNum > 60)
-                        {
-                            Console.WriteLine("The number {0} is even", userNum);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Even", userNum);
-                        }
+                        Console.WriteLine("The number {0} is odd", userNum);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Your number isn't between 1 and 100!");
+                    // Check if less than 25, or if greater than 60, or something in between
+                    if (userNum < 25)
+                    {
+                        Console.WriteLine("Even and less than 25");
+                    }
+                    else if (userNum > 60)
+                    {
+                        Console.WriteLine("The number {0} is even", userNum);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Even", userNum);
+                    }
                 }
-
-                // Continue?
-                Console.WriteLine("");
-                Console.WriteLine("Thank you for playing {0}! Would you like to play again? (y/n)", username);
-                if (Console.ReadLine() == "n")
-                {
-                    cont = false;
-                }
-                Console.WriteLine("");
             }
-            while (cont);
+            else
+            {
+                // Error message if input was outside of the range
+                Console.WriteLine("Your number isn't between 1 and 100!");
+            }
         }
 
+        static void Continue()
+        {
+            // Ask if user wants to continue
+            Console.WriteLine("");
+            Console.WriteLine("Thank you for playing {0}! Would you like to play again? (y/n)", username);
+
+            // if not we change the bool cont to false, which will end the program
+            if (Console.ReadLine() == "n")
+            {
+                cont = false;
+            }
+            Console.WriteLine("");
+        }
     }
 }
